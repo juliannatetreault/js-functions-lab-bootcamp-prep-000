1a019e42501b85f2646f2652d01fe3ae41fb58f2
Julianna Tetreault <juliannatetreault@icloud.com>
2018-07-19 09:09:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-happyHolidaysTo('Julianna')
+happyHolidaysTo(Julianna)
 
 function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-happyHolidaysTo('birthday', 'Julianna')
+happyHolidaysTo(birthday, Julianna)
 
